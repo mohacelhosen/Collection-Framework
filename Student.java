36863694dde8748e4aa0f23e8d23c2e41e99445c
mohacelhosen <mohacel.hosen22@gmail.com>
2023-02-19 11:46:15 +0600

    Student Object

--- Student.java
+++ Student.java
@@ -1,13 +1,45 @@
 public class Student {
-    String name;
-    int id;
-    float CGPA;
-    String department;
+    private String name;
+    private int id;
+    private float CGPA;
+    private String department;
 
     public Student(String name, int id, float CGPA, String department){
         this.name = name;
         this.id = id;
         this.CGPA = CGPA;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(float CGPA) {
+        this.CGPA = CGPA;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
         this.department = department;
     }
 
