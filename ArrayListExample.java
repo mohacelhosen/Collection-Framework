1b769e8d5536054acf572c3a99a0dfb914bdad8b
mohacelhosen <mohacel.hosen22@gmail.com>
2023-02-19 11:42:01 +0600

    ArrayList updated

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -6,11 +6,16 @@ public class ArrayListExample {
         System.out.println("<-------------   Welcome to the ArrayList   ------------------->");
         List<Student> al = new ArrayList<Student>();
 
+//        add element into arrayList using add() method
         al.add(new Student("Mohacel",1002,3.85f,"CSE"));
         al.add(new Student("Reznu",1024,3.55f,"CSE"));
         al.add(new Student("Asadu",1030,3.50f,"CSE"));
+        al.add(new Student("Adi",1033,3.65f,"CSE"));
 
+//        is our List is empty or not using isEmpty() method
+        System.out.println(al.isEmpty());
 
+//      Number of Element using size() method
         System.out.println("Number of elements : "+al.size());
 
 //        Access each item from Arraylist using forEach loop
